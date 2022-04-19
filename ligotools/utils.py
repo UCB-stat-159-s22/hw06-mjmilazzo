3018dd622fe9b005dbcdf1807f2a4b672c42eb6e
Michael Milazzo <mjmilazzo14@gmail.com>
2022-04-19 17:51:46 +0000

    added empty utils funcs

--- ligotools/utils.py
+++ ligotools/utils.py
@@ -1,1 +1,25 @@
 ## This is a python utils file
+
+def whiten():
+    """
+    
+    """
+    ...
+    return None
+
+
+def write_wavfile():
+    """
+
+    """
+    ...
+    return None
+
+
+def reqshift():
+    """
+
+    """
+    ...
+    return None
+
